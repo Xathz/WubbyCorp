52dc2029e6dcd03dbc5c4e21fb251783f27b8c3b
Xathz <xathz@users.noreply.github.com>
2018-12-26 17:19:26 -0500

    Initial project commit

---
+++ WubbyCorp/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WubbyCorp {
+
+    public partial class MainForm : Form {
+
+        public MainForm() {
+            InitializeComponent();
+            Icon = Properties.Resources.WubbyCorp;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            IntroForm introForm = new IntroForm();
+            introForm.Show();
+        }
+
+    }
+
+}
---
+++ WubbyCorp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WubbyCorp: The Game")]
+[assembly: AssemblyDescription("WubbyCorp: The Game")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xathz")]
+[assembly: AssemblyProduct("WubbyCorp")]
+[assembly: AssemblyCopyright("2018-2019, Xathz")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d9fa2bc-0e3c-4fba-80c5-b7e6dc2dba8e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
