5f74684d210dc8ae98574e1c3a6c628649e86db9
Xathz <xathz@users.noreply.github.com>
2018-12-29 13:02:25 -0500

    Add Company control

---
+++ WubbyCorp/Controls/Company.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WubbyCorp.Controls {
+    partial class Company {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.CompanyLogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.CompanyDisplayNameLabel = new System.Windows.Forms.Label();
+            this.CompanyTaglineLabel = new System.Windows.Forms.Label();
+            this.CompanyLevelLabel = new System.Windows.Forms.Label();
+            this.BuyButton = new System.Windows.Forms.Button();
+            this.ProductionTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.CompanyLogoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CompanyLogoPictureBox
+            // 
+            this.CompanyLogoPictureBox.ErrorImage = null;
+            this.CompanyLogoPictureBox.InitialImage = null;
+            this.CompanyLogoPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.CompanyLogoPictureBox.Name = "CompanyLogoPictureBox";
+            this.CompanyLogoPictureBox.Size = new System.Drawing.Size(75, 75);
+            this.CompanyLogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CompanyLogoPictureBox.TabIndex = 0;
+            this.CompanyLogoPictureBox.TabStop = false;
+            this.CompanyLogoPictureBox.Click += new System.EventHandler(this.CompanyLogoPictureBox_Click);
+            // 
+            // CompanyDisplayNameLabel
+            // 
+            this.CompanyDisplayNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CompanyDisplayNameLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CompanyDisplayNameLabel.Location = new System.Drawing.Point(81, 3);
+            this.CompanyDisplayNameLabel.Name = "CompanyDisplayNameLabel";
+            this.CompanyDisplayNameLabel.Size = new System.Drawing.Size(304, 21);
+            this.CompanyDisplayNameLabel.TabIndex = 1;
+            this.CompanyDisplayNameLabel.Text = "Company Display Name";
+            // 
+            // CompanyTaglineLabel
+            // 
+            this.CompanyTaglineLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CompanyTaglineLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CompanyTaglineLabel.Location = new System.Drawing.Point(89, 24);
+            this.CompanyTaglineLabel.Name = "CompanyTaglineLabel";
+            this.CompanyTaglineLabel.Size = new System.Drawing.Size(296, 15);
+            this.CompanyTaglineLabel.TabIndex = 2;
+            this.CompanyTaglineLabel.Text = "Company Tagline";
+            // 
+            // CompanyLevelLabel
+            // 
+            this.CompanyLevelLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CompanyLevelLabel.Location = new System.Drawing.Point(0, 78);
+            this.CompanyLevelLabel.Name = "CompanyLevelLabel";
+            this.CompanyLevelLabel.Size = new System.Drawing.Size(78, 15);
+            this.CompanyLevelLabel.TabIndex = 3;
+            this.CompanyLevelLabel.Text = "1";
+            this.CompanyLevelLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BuyButton
+            // 
+            this.BuyButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BuyButton.Enabled = false;
+            this.BuyButton.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BuyButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BuyButton.Location = new System.Drawing.Point(84, 55);
+            this.BuyButton.Name = "BuyButton";
+            this.BuyButton.Size = new System.Drawing.Size(301, 38);
+            this.BuyButton.TabIndex = 4;
+            this.BuyButton.Text = "Buy (x1) for 1 WubbyCado";
+            this.BuyButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.BuyButton.UseVisualStyleBackColor = true;
+            this.BuyButton.Click += new System.EventHandler(this.BuyButton_Click);
+            // 
+            // ProductionTimer
+            // 
+            this.ProductionTimer.Interval = 5000;
+            this.ProductionTimer.Tick += new System.EventHandler(this.ProductionTimer_Tick);
+            // 
+            // Subsidiary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BuyButton);
+            this.Controls.Add(this.CompanyLogoPictureBox);
+            this.Controls.Add(this.CompanyLevelLabel);
+            this.Controls.Add(this.CompanyTaglineLabel);
+            this.Controls.Add(this.CompanyDisplayNameLabel);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Subsidiary";
+            this.Size = new System.Drawing.Size(388, 97);
+            this.Load += new System.EventHandler(this.Company_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CompanyLogoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CompanyLogoPictureBox;
+        private System.Windows.Forms.Label CompanyDisplayNameLabel;
+        private System.Windows.Forms.Label CompanyTaglineLabel;
+        private System.Windows.Forms.Label CompanyLevelLabel;
+        private System.Windows.Forms.Button BuyButton;
+        private System.Windows.Forms.Timer ProductionTimer;
+    }
+}
---
+++ WubbyCorp/Controls/Company.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using WubbyCorp.Extensions;
+using WubbyCorp.Utilities;
+
+namespace WubbyCorp.Controls {
+
+    public partial class Company : UserControl {
+
+        public Company() {
+            InitializeComponent();
+
+            // Not sute if I like the wubbyCado icon on the button or not
+            //BuyButton.ImageAlign = ContentAlignment.MiddleRight;
+            //BuyButton.TextImageRelation = TextImageRelation.TextBeforeImage;
+            //BuyButton.Image = ImageUtils.ResizeImage(Properties.Resources.wubbyCado, 24, 24);
+        }
+
+        private void Company_Load(object sender, EventArgs e) {
+            SetBuyButtonText();
+            ProductionTimer.Start();
+        }
+
+        private void BuyButton_Click(object sender, EventArgs e) {
+            // TODO Subtract from the total WubbyCados
+        }
+
+        private void CompanyLogoPictureBox_Click(object sender, EventArgs e) {
+            // TODO Add to the total WubbyCados
+        }
+
+        private void ProductionTimer_Tick(object sender, EventArgs e) {
+   
+        }
+
+        private void SetBuyButtonText() {
+            (double value, string dictionaryWord) = (BuyPrice * BuyAmount).ToDictionaryWord();
+            BuyButton.Text = $"Buy (x{BuyAmount}) for {((BuyPrice > 999D) ? value.ToString("N2") : value.ToString("N0"))} {((string.IsNullOrEmpty(dictionaryWord)) ? "" : $"{dictionaryWord} ")}WubbyCado{((BuyPrice == 1D) ? "" : "s")}";
+        }
+
+        public event PropertyChangedEventHandler MultiplierChanged;
+        protected void OnMultiplierChanged(string propertyName) => MultiplierChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private int _Multiplier = 1;
+        /// <summary>
+        /// Gets or sets the multiplier used to calculate the <see cref="Production"/>.
+        /// </summary>
+        public int Multiplier {
+            get => _Multiplier;
+            set {
+                if (!(_Multiplier == value)) {
+                    _Multiplier = value;
+                    OnMultiplierChanged(nameof(Multiplier));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler ProductionIntervalChanged;
+        protected void OnProductionIntervalChanged(string propertyName) => ProductionIntervalChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private int _ProductionInterval = 5000;
+        /// <summary>
+        /// Gets or sets how often a production cycle takes in milliseconds.
+        /// </summary>
+        public int ProductionInterval {
+            get => _ProductionInterval;
+            set {
+                if (!(_ProductionInterval == value)) {
+                    _ProductionInterval = value;
+                    OnProductionIntervalChanged(nameof(ProductionInterval));
+
+                    if (!(ProductionTimer.Interval == ProductionInterval)) {
+                        if (ProductionTimer.Enabled) {
+                            ProductionTimer.Stop();
+                            ProductionTimer.Interval = ProductionInterval;
+                            ProductionTimer.Start();
+                        } else {
+                            ProductionTimer.Interval = ProductionInterval;
+                        }
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler ProductionChanged;
+        protected void OnProductionChanged(string propertyName) => ProductionChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private double _Production;
+        /// <summary>
+        /// Gets or sets how many WubbyCados to make in a single <see cref="ProductionInterval"/>.
+        /// </summary>
+        public double Production {
+            get => _Production;
+            set {
+                if (!(_Production == value)) {
+                    _Production = value;
+                    OnProductionChanged(nameof(Production));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler BuyPriceChanged;
+        protected void OnBuyPriceChanged(string propertyName) => BuyPriceChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private double _BuyPrice = 1D;
+        /// <summary>
+        /// Gets or sets the cost of the next <see cref="CompanyLevel"/> in WubbyCados.
+        /// </summary>
+        public double BuyPrice {
+            get => _BuyPrice;
+            set {
+                if (!(_BuyPrice == value)) {
+                    _BuyPrice = value;
+                    OnBuyPriceChanged(nameof(BuyPrice));
+
+                    SetBuyButtonText();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler BuyAmountChanged;
+        protected void OnBuyAmountChanged(string propertyName) => BuyAmountChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private int _BuyAmount = 1;
+        /// <summary>
+        /// Gets or sets how many <see cref="CompanyLevel"/> to buy.
+        /// </summary>
+        public int BuyAmount {
+            get => _BuyAmount;
+            set {
+                if (!(_BuyAmount == value)) {
+                    _BuyAmount = value;
+                    OnBuyAmountChanged(nameof(BuyAmount));
+
+                    SetBuyButtonText();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler BuyButtonEnabledChanged;
+        protected void OnBuyButtonEnabledChanged(string propertyName) => BuyButtonEnabledChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the control can respond to user interaction.
+        /// </summary>
+        public bool BuyButtonEnabled {
+            get => BuyButton.Enabled;
+            set {
+                if (!(BuyButton.Enabled == value)) {
+                    if (BuyButton.InvokeRequired) {
+                        Invoke(new Action(() => BuyButton.Enabled = value));
+                    } else {
+                        BuyButton.Enabled = value;
+                    }
+                    OnBuyButtonEnabledChanged(nameof(BuyButtonEnabled));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler CompanyLogoChanged;
+        protected void OnCompanyLogoChanged(string propertyName) => CompanyLogoChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Gets or sets an image for the company logo.
+        /// </summary>
+        public Image CompanyLogo {
+            get => CompanyLogoPictureBox.Image;
+            set {
+                if (!(CompanyLogoPictureBox.Image == value)) {
+                    if (CompanyLogoPictureBox.InvokeRequired) {
+                        Invoke(new Action(() => CompanyLogoPictureBox.Image = value));
+                    } else {
+                        CompanyLogoPictureBox.Image = value;
+                    }
+                    OnCompanyLogoChanged(nameof(CompanyLogo));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler CompanyDisplayNameChanged;
+        protected void OnCompanyDisplayNameChanged(string propertyName) => CompanyDisplayNameChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Gets or sets the company display name.
+        /// </summary>
+        public string CompanyDisplayName {
+            get => CompanyDisplayNameLabel.Text;
+            set {
+                if (!(CompanyDisplayNameLabel.Text == value)) {
+                    if (CompanyDisplayNameLabel.InvokeRequired) {
+                        Invoke(new Action(() => CompanyDisplayNameLabel.Text = value));
+                    } else {
+                        CompanyDisplayNameLabel.Text = value;
+                    }
+                    OnCompanyDisplayNameChanged(nameof(CompanyDisplayName));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler CompanyTaglineChanged;
+        protected void OnCompanyTaglineChanged(string propertyName) => CompanyTaglineChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Gets or sets the company tagline.
+        /// </summary>
+        public string CompanyTagline {
+            get => CompanyTaglineLabel.Text;
+            set {
+                if (!(CompanyTaglineLabel.Text == value)) {
+                    if (CompanyTaglineLabel.InvokeRequired) {
+                        Invoke(new Action(() => CompanyTaglineLabel.Text = value));
+                    } else {
+                        CompanyTaglineLabel.Text = value;
+                    }
+                    OnCompanyTaglineChanged(nameof(CompanyTagline));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler CompanyLevelChanged;
+        protected void OnCompanyLevelChanged(string propertyName) => CompanyLevelChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Gets or sets the company level.
+        /// </summary>
+        public string CompanyLevel {
+            get => CompanyLevelLabel.Text;
+            set {
+                if (!(CompanyLevelLabel.Text == value)) {
+                    if (CompanyLevelLabel.InvokeRequired) {
+                        Invoke(new Action(() => CompanyLevelLabel.Text = value));
+                    } else {
+                        CompanyLevelLabel.Text = value;
+                    }
+                    OnCompanyLevelChanged(nameof(CompanyLevel));
+                }
+            }
+        }
+
+    }
+
+}
